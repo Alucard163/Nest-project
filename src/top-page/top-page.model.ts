97b5b87a6b2166f27a56d0e88538378bd9d0cc29
alucard163 <romemix163@gmail.com>
2022-03-09 10:16:32 +0400

    initial

---
+++ src/top-page/top-page.model.ts
@@ -0,0 +1,63 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
+export class HdData {
+  @prop()
+  count: number;
+
+  @prop()
+  juniorSalary: number;
+
+  @prop()
+  middleSalary: number;
+
+  @prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+  @prop()
+  title: string;
+
+  @prop()
+  description: string;
+}
+
+export enum TopLevelCategory {
+  Courses,
+  Services,
+  Books,
+  Products,
+}
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps {
+  @prop({ enum: TopLevelCategory })
+  firstCategory: TopLevelCategory;
+
+  @prop()
+  secondCategory: string;
+
+  @prop({ unique: true })
+  alias: string;
+
+  @prop()
+  title: string;
+
+  @prop()
+  category: string;
+
+  @prop({ type: () => HdData })
+  hh?: HdData;
+
+  @prop({ type: () => [TopPageAdvantage] })
+  advantages: TopPageAdvantage[];
+
+  @prop()
+  seoText: string;
+
+  @prop()
+  tagsTitle: string;
+
+  @prop({ type: () => [String] })
+  tags: string[];
+}
