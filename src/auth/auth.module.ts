e9cf3b51fb20f061cf2aa34760be5b3d5e586a32
alucard163 <romemix163@gmail.com>
2022-04-25 20:10:14 +0400

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,9 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthService } from './auth.service';
 import { UserModel } from "./user.model";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { getJWTConfig } from "../configs/jwt.config";
 
 @Module({
   controllers: [AuthController],
@@ -15,6 +18,11 @@ import { UserModel } from "./user.model";
         },
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJWTConfig
+    })
   ],
   providers: [AuthService],
 })
