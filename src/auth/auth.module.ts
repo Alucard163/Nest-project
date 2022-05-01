cb2d3d9d3c25ad860d9af91a8d047d23ba6dfb06
alucard163 <romemix163@gmail.com>
2022-05-01 16:54:43 +0400

    finish auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthController } from './auth.controller';
+import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
-import { UserModel } from "./user.model";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { getJWTConfig } from "../configs/jwt.config";
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from '../configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStratagy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
@@ -14,16 +16,18 @@ import { getJWTConfig } from "../configs/jwt.config";
       {
         typegooseClass: UserModel,
         schemaOptions: {
-          collection: 'User',
-        },
-      },
+          collection: 'User'
+        }
+      }
     ]),
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getJWTConfig
-    })
+    }),
+    PassportModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStratagy]
 })
-export class AuthModule {}
+export class AuthModule { }
--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -5,9 +5,14 @@ import { AppModule } from './../src/app.module';
 import { CreateReviewDto } from '../src/review/dto/create-review.dto';
 import { Types, disconnect } from 'mongoose';
 import { REVIEW_NOT_FOUND } from '../src/review/review.constants';
+import { AuthDto } from "../src/auth/dto/auth.dto";
 
 const productId = new Types.ObjectId().toHexString();
 
+const loginDto: AuthDto = {
+  login: 'a@a.ru',
+  password: '1'
+}
 const testDto: CreateReviewDto = {
   name: 'Тест',
   title: 'Заголовок',
@@ -19,6 +24,7 @@ const testDto: CreateReviewDto = {
 describe('AppController (e2e)', () => {
   let app: INestApplication;
   let createdId: string;
+  let token: string;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -27,6 +33,9 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    const { body } = await request(app.getHttpServer()).post('/auth/login').send(loginDto);
+    token = body.access_token
   });
 
   it('/review/create (POST) - success', async (done) => {
@@ -71,12 +80,14 @@ describe('AppController (e2e)', () => {
   it('/review/:id (DELETE) - success', () => {
     return request(app.getHttpServer())
       .delete('/review/' + createdId)
+      .set('Authorization', 'Bearer ' + token)
       .expect(200);
   });
 
   it('/review/:id (DELETE) - fail', () => {
     return request(app.getHttpServer())
       .delete('/review/' + new Types.ObjectId().toHexString())
+      .set('Authorization', 'Bearer ' + token)
       .expect(404, {
         statusCode: 404,
         message: REVIEW_NOT_FOUND,
